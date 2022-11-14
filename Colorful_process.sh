8d42e2ef8290298d21606456e38b04eb1347debd
Arina Goncharenko <mrmekler@gmail.com>
2022-11-15 01:04:39 +0300

    Change scripts Colorful_process: Fix conditions and array

--- Colorful_process.sh
+++ Colorful_process.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-arr_symbols=('▷▷▷▷' '▶▶▶▶' '▷▷▷▷' '▶▶▶▶' '▷▷▷▷' '▶▶▶▶' '▷▷▷▷' '▶▶▶▶' '▷▷▷▷' '▶▶▶▶')
+arr_symbols=('▷▷▷▷' '▶▶▶▶')
 arr_foreground_colours=(107 40 107 40 107 40 107 40 107 40 107 40)
 arr_background_colours=(30 97 30 97 30 97 30 97 30 97 30 97)
+
 space=" "
 index=0
-for ((index_w=0; index_w < ${#arr_symbols[@]}; index_w++)); do
-  abs=$(expr $index_w % 2)
-  if [[ $abs == 0 ]]; then
-    echo -en "\r$space\e[${arr_foreground_colours[index]}m\e[${arr_background_colours[index]}m ${arr_symbols[index_w]} \e[0m"
-    sleep .5
-  else
-    echo -en "\r$space\e[${arr_foreground_colours[index]}m\e[${arr_background_colours[index]}m ${arr_symbols[index_w]} \e[0m"
-    sleep .5
-  fi
- space="$space      "
+for ((index=0; index < ${#arr_foreground_colours[@]}; index++)); do
+  for symbol in ${arr_symbols[@]}; do
+    if [[ $symbol == "${arr_symbols[0]}" ]]; then
+      echo -en "\r$space\e[${arr_foreground_colours[index]}m\e[${arr_background_colours[index]}m $symbol \e[0m"
+      sleep .5
+    else
+      echo -en "\r$space\e[${arr_foreground_colours[index+1]}m\e[${arr_background_colours[index+1]}m $symbol \e[0m"
+      sleep .5
+    fi
+    space="$space      "
+  done
   let index=$index+1
 done
